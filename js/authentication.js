de630a1ccad9727db5a3927805d2b0367c83700e
AcademyRS <39175254+AcademyRS@users.noreply.github.com>
2018-07-20 20:54:38 -0300

    Add files via upload

---
+++ js/authentication.js
@@ -0,0 +1,16 @@
+let btnLogin = document.querySelector('#submit');
+
+//Inputs
+
+let email = document.querySelector('#email');
+let password = document.querySelector('#password');
+
+
+btnLogin.addEventListener('click', function () {
+    firebase.auth().signInWithEmailAndPassword(email.value, password.value).then(function (result) {
+        console.log(result);
+        window.location.replace('proj.html');
+    }).catch(function (error) {
+        console.log(error);
+    });
+});

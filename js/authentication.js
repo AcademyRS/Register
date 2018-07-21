8731c5c0fd7dcf99c0d1f357784877cb6f66a6a1
AcademyRS <39175254+AcademyRS@users.noreply.github.com>
2018-07-21 01:35:25 -0300

    Update authentication.js

--- js/authentication.js
+++ js/authentication.js
@@ -9,7 +9,7 @@ let password = document.querySelector('#password');
 btnLogin.addEventListener('click', function () {
     firebase.auth().signInWithEmailAndPassword(email.value, password.value).then(function (result) {
         console.log(result);
-        window.location.replace('https://academyrs.github.io/Register/proj.html');
+//         window.location.replace('https://academyrs.github.io/Register/proj.html');
     }).catch(function (error) {
         console.log(error);
     });
